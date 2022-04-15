b640eeaa191d03af1ca2531e18ef3d1bbdaec718
Caleb M Rank <calebmrank@gmail.com>
2022-04-15 10:17:16 -0600

    commit 04/15/2022

---
+++ 04-singly_linked_lists/03-length/index.js
@@ -0,0 +1,67 @@
+class Node {
+    constructor( data=null ) {
+        this.data = data
+        this.next = null
+    };
+};
+
+class SLL {
+    constructor() {
+        this.head = null
+    };
+
+    // ================= 01-fronts
+    front() {
+        if ( !this.head ) { return null };
+        return this.head.data;
+    };
+    addFront( val ) {
+        let newNode = new Node( val );
+
+        if ( !this.head ) {
+            this.head = newNode;
+            return this.head;
+        };
+
+        newNode.next = this.head;
+        this.head = newNode;
+        return this.head;
+    };
+    removeFront() {
+        if ( !this.head ) { return null }
+        else if ( !this.head.next ) {
+            this.head = null;
+            return this.head;
+        }
+
+        let temp = this.head.next;
+        delete this.head;
+        this.head = temp;
+        return this.head;
+    };
+    
+    // ================= 02-contains
+    contains( value ) {
+        if ( !this.head ) { return false; }
+
+        let runner = this.head;
+        while ( !runner.next ) {
+            if ( runner.data == value ) { return true; }
+            runner = runner.next;
+        };
+        return false;
+    };
+    
+    // ================= 03-length
+    length() {
+        if ( !this.head ) { return 0; }
+
+        let runner = this.head;
+        let length = 0;
+        while ( !runner.next ) {
+            length++
+            runner = runner.next;
+        };
+        return length;
+    };
+};
