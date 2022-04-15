620e0e02e50dab0d386609db21a0b975efc11945
Caleb M Rank <calebmrank@gmail.com>
2022-04-15 09:59:56 -0600

    commit 04/15/2022

---
+++ 04-singly_linked_lists/01-fronts/index.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor( data=null ) {
+        this.data = data
+        this.next = null
+    };
+};
+
+class SLL {
+    constructor() {
+        this.head = null
+    };
+
+    front() {
+        if ( !this.head ) { return null };
+        return this.head.data;
+    };
+    addFront( val ) {
+        let newNode = new Node( val );
+
+        if ( !this.head ) {
+            this.head = newNode;
+            return this.head;
+        };
+
+        newNode.next = this.head;
+        this.head = newNode;
+        return this.head;
+    };
+    removeFront() {
+        if ( !this.head ) { return null }
+        else if ( !this.head.next ) {
+            this.head = null;
+            return this.head;
+        }
+
+        let temp = this.head.next;
+        delete this.head;
+        this.head = temp;
+        return this.head;
+    };
+};
