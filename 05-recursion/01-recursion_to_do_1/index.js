a733ee9c03d4155d39491e00bb9a7eb500120a8b
Caleb M Rank <calebmrank@gmail.com>
2022-04-19 08:59:53 -0600

    commit 04/19/2022

---
+++ 05-recursion/01-recursion_to_do_1/index.js
@@ -0,0 +1,22 @@
+// ================= 01-to_do_1
+const rSigma = num => {
+    if ( num < 1 ) {
+        return 0;
+    };
+    
+    return rSigma( Math.floor(num-1) ) + Math.floor(num);
+};
+const rFact = num => {
+    if ( num <= 1 ) {
+        return 1;
+    };
+    
+    return rFact( Math.floor(num-1) ) * Math.floor(num);
+};
+
+console.log( `rSigma(5): ${rSigma(5)}` );
+console.log( `rSigma(2.5): ${rSigma(2.5)}` );
+console.log( `rSigma(-1): ${rSigma(-1)}` );
+
+console.log( `rFact(3): ${rFact(3)}` );
+console.log( `rFact(6.5): ${rFact(6.5)}` );
